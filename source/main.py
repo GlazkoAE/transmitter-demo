5d37a251e69db07f93997242cb2457aec2cf920e
Andrew Glazko <glazko.ae@gmail.com>
2022-07-05 12:01:52 +0300

    add N bytes per symbol processing

--- source/main.py
+++ source/main.py
@@ -1,6 +1,3 @@
-# TODO: move constants to arguments with argparse
-# TODO: think about transmitting data from file
-
 import serial
 import PySimpleGUI as sg
 import uart
@@ -11,12 +8,16 @@ from gui import GUI, Axes
 def main():
     # sudo chmod 666 /dev/ttyUSB0
     baud_rate = 115200
-    block_length_in_bits = 2 ** 12
-    welch_nperseg = 512
+    block_length_in_bits = 2 ** 14
+    welch_nperseg = 1024
+    bytes_per_sample = 2
     is_single_byte = False
     block_length_in_bytes = int(block_length_in_bits / 8)
     filter_coefficients = [6, 3, -4, -10, -8, 2, 16, 23, 13, -13, -40, -47, -15, 57, 149, 226, 255, 226, 149, 57, -15,
                            -47, -40, -13, 13, 23, 16, 2, -8, -10, -4, 3, 6]
+    filter_power = sum(x ** 2 for x in filter_coefficients)
+    # filter_power_norm_coeff = filter_power ** 0.5
+    zeros_head_and_tail = [0 for _ in filter_coefficients]
     sps = 4
 
     gui = GUI(resolution='FHD', location=(0, 100), title='Matrix Wave - Transmit Demo')
@@ -40,15 +41,19 @@ def main():
                 frequency, psd = processing.psd(rx_signal, 1e6, nperseg=welch_nperseg)
 
                 # Parse signal to IQ signal
-                i_signal, q_signal = processing.parce_real_imag(rx_signal, sps)
+                i_signal, q_signal = processing.parce_real_imag(rx_signal, bytes_per_sample)
+                i_signal = zeros_head_and_tail + i_signal + zeros_head_and_tail
+                q_signal = zeros_head_and_tail + q_signal + zeros_head_and_tail
 
                 # Filter IQ signal to get IQ samples
-                i_samples = processing.filter_signal(i_signal, filter_coefficients, sps)
-                q_samples = processing.filter_signal(q_signal, filter_coefficients, sps)
+                i_samples = processing.filter_signal(i_signal, filter_coefficients, sps) / filter_power
+                q_samples = processing.filter_signal(q_signal, filter_coefficients, sps) / filter_power
 
                 # Update plots
                 ax_const.draw(x=i_samples, y=q_samples, style='ro')
                 ax_psd.draw(x=frequency, y=psd, style='b')
+                # ax_const.draw(x=range(len(i_samples)), y=i_samples, style='bo-')
+                # ax_psd.draw(x=range(len(i_signal)), y=i_signal, style='b-')
 
 
 if __name__ == '__main__':
--- source/processing.py
+++ source/processing.py
@@ -2,15 +2,44 @@ from scipy.signal import upfirdn
 from scipy.signal import welch
 
 
-def parce_real_imag(data: list[int], sps: int):
-    i_sig = []
-    q_sig = []
-    for sample_start_index in range(0, len(data), sps * 2):
-        i_last = sample_start_index + sps
-        q_last = sample_start_index + sps * 2
-        i_sig += data[sample_start_index:i_last]
-        q_sig += data[i_last:q_last]
-    return i_sig, q_sig
+def parse_bytes_reversed(sample, bytes_num):
+    res = 0
+    for index, reversed_index in enumerate(reversed(range(bytes_num))):
+        coeff = 2 ** (8 * index)
+        res += sample[reversed_index] * coeff
+    return res
+
+
+def parse_bytes(sample, bytes_num):
+    res = 0
+    for index in range(bytes_num):
+        coeff = 2 ** (8 * index)
+        res += sample[index] * coeff
+    return res
+
+
+def parse_samples(signal, bytes_num):
+    sample = []
+    res = []
+    for byte in signal:
+        sample.append(byte)
+        if len(sample) == bytes_num:
+            res.append(parse_bytes(sample, bytes_num))
+            sample = []
+    return res
+
+
+def parce_real_imag(data: list[int], bytes_per_sample=1):
+    i_samples = []
+    q_samples = []
+    for sample_start_index in range(0, len(data), 2 * bytes_per_sample):
+        i_last = sample_start_index + bytes_per_sample
+        q_last = sample_start_index + bytes_per_sample * 2
+        i_sig = data[sample_start_index:i_last]
+        q_sig = data[i_last:q_last]
+        i_samples += parse_samples(i_sig, bytes_per_sample)
+        q_samples += parse_samples(q_sig, bytes_per_sample)
+    return i_samples, q_samples
 
 
 def filter_signal(signal, coefficients, sps):
